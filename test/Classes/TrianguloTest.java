8e3b2180e471cca00a3568947e9eaf5b18a7ddf8
picinin <picinin@luis>
2019-03-01 09:50:49 -0300

    +-, tá carregando os testes do arquivo, mais tá estorando exceção quando dá erro

--- test/Classes/TrianguloTest.java
+++ test/Classes/TrianguloTest.java
@@ -5,13 +5,17 @@
  */
 package Classes;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -19,26 +23,26 @@ import static org.junit.Assert.*;
  */
 public class TrianguloTest
 {
-    
+
     public TrianguloTest()
     {
     }
-    
+
     @BeforeClass
     public static void setUpClass()
     {
     }
-    
+
     @AfterClass
     public static void tearDownClass()
     {
     }
-    
+
     @Before
     public void setUp()
     {
     }
-    
+
     @After
     public void tearDown()
     {
@@ -48,8 +52,32 @@ public class TrianguloTest
      * Test of VerificarTipo method, of class Triangulo.
      */
     @Test
-    public void testVerificarTipo()
+    public void testVerificarTipo() throws IOException
     {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(
+                new FileReader("/home/picinin/NetBeansProjects/JavaJunit/src/Arquivo/triangulo.csv")))
+        {
+            String line = br.readLine();
+            while ((line = br.readLine()) != null)
+            {
+                try
+                {
+                    String[] values = line.split(";");
+                    Triangulo t = new Triangulo();
+                    //System.out.println(line);
+                    Assert.assertEquals(values[3],
+                            t.VerificarTipo(Integer.parseInt(values[0]),
+                                    Integer.parseInt(values[1]),
+                                    Integer.parseInt(values[2])));
+                }catch(Exception ex)
+                {
+                    //System.out.println(ex.getMessage());
+                }
+
+                //records.add(Arrays.asList(values));
+            }
+        }
         /*System.out.println("VerificarTipo");
         int l1 = 0;
         int l2 = 0;
@@ -63,5 +91,5 @@ public class TrianguloTest
         Triangulo t = new Triangulo();
         Assert.assertEquals("Isósceles", t.VerificarTipo(1, 2, 2));
     }
-    
+
 }
