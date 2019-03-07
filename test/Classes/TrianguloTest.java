c1996d3c822366d4aa605998602dab989e0337b2
Aluno <Aluno@LF104m15-72129>
2019-03-07 09:24:53 -0300

    Chico God

--- test/Classes/TrianguloTest.java
+++ test/Classes/TrianguloTest.java
@@ -5,22 +5,13 @@
  */
 package Classes;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import junitparams.FileParameters;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 /**
  *
@@ -53,48 +44,11 @@ public class TrianguloTest
     {
     }
 
-    public static List<String> getLines() throws IOException
-    {
-        List<String> l = new ArrayList<>();
-        try
-        {
-
-            BufferedReader br = new BufferedReader(
-                    new FileReader("C:\\Users\\Aluno\\Documents\\NetBeansProjects\\JavaJunit\\src\\Arquivo\\triangulo.csv"));
-            String line;
-            while ((line = br.readLine()) != null)
-            {
-                l.add(line);
-            }
-        } catch (Exception ex)
-        {
-            System.out.println("");
-        }
-        return l;
-    }
-
-    //https://www.wedoqa.com/2014/03/reading-test-data-from-a-csv-file-using-junit/
     @Test
-    public void testVerificarTipo()
+    @FileParameters("src\\Arquivo\\triangulo.csv")
+    public void VerificarTipo(int l1, int l2, int l3, String resultado)
     {
-        List<String> l = new ArrayList<>();
-        try
-        {
-            l = getLines();
-        } catch (IOException ex)
-        {
-            Logger.getLogger(TrianguloTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        String[] line;
         Triangulo t = new Triangulo();
-        for (int i = 0; i < l.size(); i++)
-        {
-            line = l.get(i).split(";");
-
-            Assert.assertEquals(line[3],
-                    t.VerificarTipo(Integer.parseInt(line[0]),
-                            Integer.parseInt(line[1]),
-                            Integer.parseInt(line[2])));
-        }
+        Assert.assertEquals(resultado, t.VerificarTipo(l1, l2, l3));
     }
 }
